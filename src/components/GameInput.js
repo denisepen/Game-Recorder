6c43f936ceac6f1e1815ae34d95d05f002a539d0
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-06 18:26:48 -0400

    added onchange funcs for comments, mode, etc.

--- src/components/GameInput.js
+++ src/components/GameInput.js
@@ -11,8 +11,22 @@ export default class GameInput extends Component {
     }
   }
 
-handlechange = () => {
+handleModeChange = (e) => {
+  this.setState({
+    mode: e.target.value
+  })
+}
 
+handleKillsChange = (e) => {
+  this.setState({
+    max_kills: e.target.value
+  })
+}
+
+handleCommentsChange = (e) => {
+  this.setState({
+    comments: e.target.value
+  })
 }
 
   render(){
@@ -20,9 +34,9 @@ handlechange = () => {
       <div>
         <h2> Please add your game </h2>
         <form>
-          Mode: <input type="text" onChange={this.handleChange}/><br/>
-          Max Kills: <input type="text" /><br/>
-          Comments: <input type="text" /><br/>
+          Mode: <input type="text" onChange={(e) => this.handleModeChange(e)} value={this.state.mode}/> {this.state.mode}<br/>
+          Max Kills: <input type="number" onChange={(e) => this.handleKillsChange(e)}/>{this.state.max_kills}<br/>
+          Comments: <input type="text" onChange={(e) => this.handleCommentsChange(e)}/>{this.state.comments}<br/>
           <input type="submit" value="submit" />
         </form><br/>
       </div>
