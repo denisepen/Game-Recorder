bf6e0371afbbf03a953d6ebccc76c8a231077972
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-06 22:37:45 -0400

    changed game list format from lost to table

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 const Games = props => {
-  const games = props.games.map((game, index) => {
-    return <li key={index}>{game.mode} Kills:{game.max_kills} {game.comments}</li>;
-  });
+  const games = props.games.map((game, index) => 
+
+      <tr key={index}>
+        <td> {game.mode} </td>
+        <td> {game.max_kills} </td>
+        <td> {game.comments}</td>
+      </tr>
+
+  );
 
   return (
     <div>
-      <ul>
-        {games}
-      </ul>
+      <table>
+        <tr>
+          <th>Game Mode</th>
+          <th>Max Kills</th>
+          <th>Comments</th>
+        </tr>
+
+          {games}
+
+      </table>
+
     </div>
   );
 
 };
 
 export default Games;
+
+// <li key={index}>{game.mode} Kills:{game.max_kills} {game.comments}</li>;
+// <ul>
+//   {games}
+// </ul>
